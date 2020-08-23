4e003eb8188fe163c49fb579ab725bde9e720d4c
dhananjaichitranshi <dhananjai.chitranshi@qsstechnosoft.com>
2020-08-23 16:27:01 +0530

    Changes

--- dist/repositories/book.repository.d.ts
+++
@@ -1,6 +0,0 @@
-import { DefaultCrudRepository } from '@loopback/repository';
-import { Book, BookRelations } from '../models';
-import { MongodsDataSource } from '../datasources';
-export declare class BookRepository extends DefaultCrudRepository<Book, typeof Book.prototype.isbn, BookRelations> {
-    constructor(dataSource: MongodsDataSource);
-}
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,2 +1,1 @@
-export * from './book.repository';
 export * from './user.repository';
